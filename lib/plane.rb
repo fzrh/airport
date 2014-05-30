35e6e3f8d21a1cca894f9ced4d74adad8e52973d
Faezrah Rizalman <fwrizalman@gmail.com>
2014-05-30 20:41:08 +0100

    Added Plane class

---
+++ lib/plane.rb
@@ -0,0 +1,23 @@
+class Plane
+
+	def initialize
+		@status = "flying"
+	end
+
+	def status
+		@status
+	end
+
+	def in_the_air
+		@status
+	end
+
+	def take_off
+		@status
+	end
+
+	def landed
+		@status = "landed"
+	end
+
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,37 @@
+require 'plane'
+
+# When we create a new plane, it should have a "flying" status, thus planes can not be created in the airport.
+#
+# When we land a plane at the airport, the plane in question should have its status changed to "landed".
+#
+# When the plane takes of from the airport, the plane's status should become "flying".
+
+describe Plane do
+
+  let(:plane) { Plane.new }
+  
+  it 'has a flying status when created' do
+  	expect(plane.status).to eq 'flying'
+  end
+  
+  it 'has a flying status when in the air' do
+    plane.in_the_air
+    expect(plane.status).to eq 'flying'
+  end
+  
+  it 'can take off' do
+    expect(plane).to receive(:take_off)
+    plane.take_off
+  end
+  
+  it 'changes its status to "flying" after taking of' do
+    plane.take_off
+    expect(plane.status).to eq 'flying'
+  end
+
+  it 'changes its status to "landed" after landing' do
+    plane.landed
+    expect(plane.status).to eq 'landed'
+  end
+
+end
