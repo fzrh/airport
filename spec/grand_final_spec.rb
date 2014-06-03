b091f2e5bec27ac7d15478978d4e554196b48c7f
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-03 22:34:26 +0100

    Updated with renamed variables

--- spec/grand_final_spec.rb
+++ spec/grand_final_spec.rb
@@ -16,15 +16,15 @@ describe 'The grand finale' do
   it 'all planes can land and change status to "landed"' do
   	hangar = double :hangar
    	6.times {plane}
- 	6.times {airport.gives_permission_to_land_to plane}
+ 	6.times {airport.give_permission_to_land_to plane}
  	expect(airport.planes_count).to eq 6
-  	expect(6.times {plane.landed!}).to be_true
+  	expect(6.times {plane.land!}).to be_true
   end
 
   it 'all planes can take off and change status to "flying"' do
   	hangar = double :hangar
   	6.times {plane}
-  	6.times {airport.gives_permission_to_take_off_to plane}
+  	6.times {airport.give_permission_to_take_off_to plane}
   	expect(airport.planes_count).to eq 0
   	expect(6.times {plane.status?}).to be_true
   end
